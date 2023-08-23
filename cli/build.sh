ed797b72f9f8f356667ca936f39ccad0b02a1801
Santhosh Karuhatty <santhosh.karuhatty@JJY569YPVQ.corp.couchbase.com>
2023-08-23 16:06:07 -0700

    Fixed all deps and added build.sh

---
+++ cli/build.sh
@@ -0,0 +1,73 @@
+echo "++++++ Running ... /opt/homebrew/bin/python3.8 -m venv .venv "
+/opt/homebrew/bin/python3.8 -m venv .venv
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running ... source .venv/bin/activate"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+source .venv/bin/activate
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running ... pip install -r requirements.txt "
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+pip install -r requirements.txt
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running pip install -r requirements-dev.txt"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+pip install -r requirements-dev.txt
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running ... pip install -e ."
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+pip install -e .
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running ... pip install aws-codeseeder"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+pip install aws-codeseeder
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running ... codeseeder deploy seedkit orbit"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+codeseeder deploy seedkit orbit
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running orbit init -n "sky-orbit-env" -r us-east-1"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+orbit init -n "sky-orbit-env" -r us-east-1
+cp -R /tmp/default-env-manifest.yaml conf/
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running orbit deploy toolkit -f conf/default-env-manifest.yaml"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+orbit deploy toolkit -f conf/default-env-manifest.yaml
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running orbit deploy images -f conf/default-env-manifest.yaml"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+orbit deploy images -f conf/default-env-manifest.yaml
+
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++ Running orbit deploy env -f conf/default-env-manifest.yaml"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+orbit deploy env -f conf/default-env-manifest.yaml
